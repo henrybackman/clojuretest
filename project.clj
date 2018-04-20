9aae0b660f4cc81a0d02eb6bdd5248b4dce9f431
Henry Backman <henry.backman@gmail.com>
2018-04-20 08:30:24 +0300

    tutorial exercises

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject clojuretest "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Test project"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+    [clj-http "2.0.0"]]
   :main ^:skip-aot clojuretest.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/clojuretest/core.clj
+++ src/clojuretest/core.clj
@@ -1,7 +1,77 @@
 (ns clojuretest.core
   (:gen-class))
 
+(require '[clj-http.client :as http])
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "TESTIONG")
+  (def greet (fn [] (println "Hello")))
+  (greet)
+  (def greet2 #(println "Hello2"))
+  (greet2)
+  (defn greet3
+    ([] (str "Hello, World!"))
+    ([x] (str "Hello, " x "!"))
+    ([x y] (str x ", " y "!"))
+  )
+  (println (greet3 "Good morning" "Clojure"))
+  ;; For testing
+  (assert (= "Hello, World!" (greet3)))
+  (assert (= "Hello, Clojure!" (greet3 "Clojure")))
+  (assert (= "Good morning, Clojure!" (greet3 "Good morning" "Clojure")))
+
+  (defn do-nothing [x] x)
+  (println (do-nothing "THIS"))
+  (assert (= "THIS" (do-nothing "THIS")))
+
+  (defn always-thing [& x] :thing)
+  (println (always-thing "THIS"))
+  (assert (= :thing (always-thing "THIS")))
+
+
+  (defn make-thingy [x] (fn [& y] x))
+  ;; Tests
+  (let [n (rand-int Integer/MAX_VALUE)
+  f (make-thingy n)]
+  (assert (= n (f)))
+  (assert (= n (f :foo)))
+  (assert (= n (apply f :foo (range)))))
+
+  (defn triplicate [f] (f) (f) (f))
+
+  (triplicate #(println "TIMES"))
+
+  (defn opposite [f]
+    (fn [& args] (not (f))))
+  (opposite #([] "what"))
+
+  (defn triplicate2 [f & args]
+    (triplicate #(f args)))
+
+  (triplicate2 println "TIMES2")
+
+  (println (Math/cos Math/PI))
+
+  (defn check-proof [x] (+ (Math/pow (Math/sin x) 2) (Math/pow (Math/cos x) 2)))
+  (println (check-proof 5))
+
+  (defn http-get [url]
+    (slurp url))
+  
+  (assert (.contains (http-get "http://www.w3.org") "html"))
+
+  (def list ["abc" false 99])
+  (println (get list 0))
+  (println (count list))
+
+  (def list2 (conj list ["new" "stuff"] "even newer"))
+  (println list2)
+
+  (def scores {"fred" 100 "bob" 200})
+  (println scores)
+  (def scores-updated (assoc scores "sall" 300))
+  (println scores-updated)
+  (println (scores "fred"))
+)
+
